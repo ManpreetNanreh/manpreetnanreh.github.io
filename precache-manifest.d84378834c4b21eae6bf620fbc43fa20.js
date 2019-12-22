94106d5d5f04dc0111a5e29f48bcd7aa8331a0db
Manpreet Nanreh <manpreetsnanreh@gmail.com>
2019-12-21 21:04:16 -0500

    Updates

---
+++ precache-manifest.d84378834c4b21eae6bf620fbc43fa20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5bd3b8a973a13b4513a7acd8642315",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "598529ee4e202bd58477",
+    "url": "/personal-website/static/css/main.3e0d47cf.chunk.css"
+  },
+  {
+    "revision": "5db030bf706eacc8529c",
+    "url": "/personal-website/static/js/2.a1807c71.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/personal-website/static/js/2.a1807c71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "598529ee4e202bd58477",
+    "url": "/personal-website/static/js/main.1855506b.chunk.js"
+  },
+  {
+    "revision": "117e3e5d63e1498f83cb",
+    "url": "/personal-website/static/js/runtime-main.c71e6253.js"
+  },
+  {
+    "revision": "4957eb4522f284e695bba196cb82c10d",
+    "url": "/personal-website/static/media/alexandre-godreau-_YEBZiTgZS8-unsplash.4957eb45.jpg"
+  }
+]);
